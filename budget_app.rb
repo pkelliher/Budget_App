ed5043005bee17ce821cc41ad04b2e3a021d4461
pkelliher <pkelliher74@gmail.com>
2020-12-14 12:59:45 -0800

    Comment inclusion

--- budget_app.rb
+++ budget_app.rb
@@ -10,8 +10,8 @@ debt1, debt2 = debt_amounts.split
 puts "Thank you, based on your entries, your first amount due is $#{debt1} and your second debt amount is $#{debt2}.
 Your total debt amount is $#{debt1.to_i + debt2.to_i}."
 
-# Go over with David the best method for counting entries in a string similar to what is outlined in 
-# this Stack Overflow article https://stackoverflow.com/questions/1416059/best-way-to-count-words-in-a-string-in-ruby
+#Go over with David the best method for counting entries in a string similar to what is outlined in 
+#this Stack Overflow article https://stackoverflow.com/questions/1416059/best-way-to-count-words-in-a-string-in-ruby 
 
 # Enter debt amount (in dollars)
 puts "Type in your total debt amount?"
@@ -30,4 +30,7 @@ puts "Thank you, I can see your total monthly income is $#{total_monthly_income}
 # How many months does it take to pay off the debt?
 
 puts "The total time it will take for you to pay off your debt is #{total_debt.to_i / total_monthly_income.to_i} months."
+
+# Create option box for debt entries and amount owed within the CLI.
+
 
