c3ac6f7e6bccbf27ce91a224226c44d4e23f2f12
pkelliher <pkelliher74@gmail.com>
2021-03-06 21:50:17 -0800

    comment sum change

--- budget_app.rb
+++ budget_app.rb
@@ -90,7 +90,7 @@ class Application
         input_arr << user_input.to_f
       end
     end
-    # This is storing the sum of all elements (amounts) in the input_arr (input array) in a variable called total_cc_debt (Total credit card debt).
+    # This stores the sum of all elements (amounts) in the input_arr (input array) in a variable called total_cc_debt (Total credit card debt).
     @total_cc_debt = input_arr.sum
 
     # This displays the user's total credit card debt amount.
