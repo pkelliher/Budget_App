2c3a23bccf519bc05d24425865c9a0535580c4ba
pkelliher <pkelliher74@gmail.com>
2021-04-04 18:08:23 -0700

    comment change

--- budget_app.rb
+++ budget_app.rb
@@ -101,7 +101,7 @@ class Application
 
 
   def enter_income
-    # Prints the the text below where a user can enter in income amounts if multiple amounts.
+    # Prints the the text below to prompt a user to enter in income dollar amounts and multiple dollar amounts if availble.
     puts "Please enter income amounts here."
 
     # Prompts the user to enter total monthly income amount.
