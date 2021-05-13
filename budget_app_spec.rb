4ce7ea9ec2a91ed62ecbc2789394941f86d8a4a1
pkelliher <pkelliher74@gmail.com>
2021-05-12 20:26:08 -0700

    Comment

--- budget_app_spec.rb
+++ budget_app_spec.rb
@@ -13,7 +13,7 @@ class Foo
     end
 end
 
-# RSpec unit BDD testing for budget app
+# RSpec unit BDD testing for budget application
 RSpec.describe "BudgetApp" do
     describe "#start" do
         it "displays the welcome message" do
