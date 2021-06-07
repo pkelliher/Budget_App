631ad81d526338705c49f7abc4086c148b081734
pkelliher <pkelliher74@gmail.com>
2021-06-06 19:20:04 -0700

    comment update

--- budget_app_spec.rb
+++ budget_app_spec.rb
@@ -32,7 +32,7 @@ RSpec.describe "BudgetApp" do
     end
 #foo method
     describe "Foo" do
-        #speak method referenace
+        #speak method ref
         describe ".speak" do
             it "speaks" do
                 allow(Foo).to receive(:speak2).and_call_original
